72f69274a5fc2662270779b395521f9b5a779f3c
Magnus <magJ@users.noreply.github.com>
2018-03-08 11:46:09 +1100

    Make async benchmark actually async

--- src/main/java/com/networknt/techempower/handler/QueriesAsyncPostgresqlGetHandler.java
+++ src/main/java/com/networknt/techempower/handler/QueriesAsyncPostgresqlGetHandler.java
@@ -10,62 +10,39 @@ import com.networknt.techempower.model.World;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
-import io.undertow.util.SameThreadExecutor;
+import rx.Observable;
+
 
 import java.nio.ByteBuffer;
-import java.util.concurrent.CountDownLatch;
 
 import static com.networknt.techempower.Helper.randomWorld;
 
 public class QueriesAsyncPostgresqlGetHandler implements HttpHandler {
     private final Db db = PostgresStartupHookProvider.db;
-    private static DslJson<Object> dsl = new DslJson<>();
-    private static JsonWriter writer = dsl.newWriter(1024);
+    private DslJson<Object> dsl = new DslJson<>();
+    private JsonWriter writer = dsl.newWriter(1024);
 
-    private static class ResultHandler implements HttpHandler {
-        private final CountDownLatch cdl;
-        private final World[] worlds;
-        public ResultHandler(CountDownLatch cdl, World[] worlds){
-            this.cdl = cdl;
-            this.worlds = worlds;
-        }
-
-        @Override
-        public void handleRequest(HttpServerExchange exchange) throws Exception {
-            if(cdl.getCount() != 0){
-                exchange.dispatch(this);
-                return;
-            }
-
-
-            writer.reset();
-            writer.serialize(worlds);
-
-            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-            exchange.getResponseSender().send(ByteBuffer.wrap(writer.toByteArray()));
-
-        }
-    }
 
     @Override
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
-        int queries = Helper.getQueries(exchange);
+    public void handleRequest(HttpServerExchange exchange) {
 
-        World[] worlds = new World[queries];
-        CountDownLatch cdl = new CountDownLatch(queries);
-        for (int i = 0; i < queries; i++) {
-            int worldIndex = i;
-            int randomWorld = randomWorld();
-            db.querySet("SELECT * FROM World WHERE id = " + randomWorld)
-                    .subscribe((resultSetContainer) -> {
-                        Row resultSet = resultSetContainer.row(0);
-                        int id = resultSet.getInt("id");
-                        int randomNumber = resultSet.getInt("randomNumber");
-                        worlds[worldIndex] = new World(id, randomNumber);
-                        cdl.countDown();
-                    }, System.err::println);
-        }
-
-        exchange.dispatch(new ResultHandler(cdl, worlds));
+        exchange.dispatch();
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+        Observable.range(0, Helper.getQueries(exchange))
+                .flatMap(i -> db.querySet("SELECT * FROM World WHERE id = $1", randomWorld()))
+                .map(resultSet -> {
+                    Row row = resultSet.row(0);
+                    int id = row.getInt("id");
+                    int randomNumber = row.getInt("randomNumber");
+                    return new World(id, randomNumber);
+                })
+                .toList()
+                .subscribe(worlds -> {
+                    System.out.println("Before control reaches here.");
+                    writer.reset();
+                    writer.serialize(worlds);
+                    exchange.getResponseSender().send(ByteBuffer.wrap(writer.toByteArray()));
+                }, System.err::println);
+        System.out.println("We return from here");
     }
 }
